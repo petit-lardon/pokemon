94a343af6086f65ae3fb958f9f0a97d61fac54de
jlavie <lavie.jul@gmail.com>
2019-07-02 23:30:00 +0200

    feat(directive): add parameter to directive

--- src/app/directives/border-card.directive.ts
+++ src/app/directives/border-card.directive.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
     selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
+    private initialColor: string = '#f5f5f5';
+    private defaultColor: string = '#009688';
+    private defaultHeigh: number = 180;
+
     constructor(private el: ElementRef) {
-        this.setBorder('#f5f5f5');
-        this.setHeight(180);
+        this.setBorder(this.initialColor);
+        this.setHeight(this.defaultHeigh);
     }
+
+    @Input('pkmnBorderCard') borderColor: string;
 
     private setBorder(color: string) {
         let border = 'solid 4px ' + color;
@@ -19,10 +25,10 @@ export class BorderCardDirective {
     }
 
     @HostListener('mouseenter') onMouseEnter() {
-        this.setBorder('#009688');
+        this.setBorder(this.borderColor || this.defaultColor);
     }
 
     @HostListener('mouseleave') onMouseLeave() {
-        this.setBorder('#f5f5f5');
+        this.setBorder(this.initialColor);
     }
 }
